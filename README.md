0431a0318a5a274b8e6e1c0d1fe387a60ec21601
rreyneke <rreyneke@g.harvard.edu>
2017-03-01 17:39:15 -0800

    Update README file; added index file with an image.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # hello-world
 # Practice repository for DWA
+http://dwa15.com
---
+++ index.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>index</title>
+</head>
+<h1>Hello World!</h1>
+<img src='/images/kitten.jpeg' alt='Adorable kitten'>
+<body>
+</body>
+</html>
